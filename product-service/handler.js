86ed173f3178f5959bd9370198b309c03c11dc81
Kristof Poszet <kristof_poszet@epam.com>
2024-03-18 17:45:53 +0100

    chore: get mock products list via API gateway

--- product-service/handler.js
+++ product-service/handler.js
@@ -1,18 +1,10 @@
 'use strict';
 
-module.exports.getProductsList = async (event) => {
+const { products } = require('./mocks/data');
+
+module.exports.getProductsList = async (_event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'My first serverless deploy.',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(products, null, 2),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
