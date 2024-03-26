abded2c17b0d9c35fdb081538c0286ab6b5c0d61
Kristof Poszet <kristof_poszet@epam.com>
2024-03-26 17:56:06 +0100

    feat: getProductsListLambda - products list from db

--- product-service/src/functions/products/lambda/getProductsListLambda.ts
+++ product-service/src/functions/products/lambda/getProductsListLambda.ts
@@ -1,9 +1,21 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 import { formatJSONResponse } from '@libs/api-gateway';
-import { products } from '../../../../mocks/data';
 import { middyfy } from '@libs/lambda';
+import { TABLE_NAMES } from '../../../../constants';
+
+const dynamoDb = new DynamoDBClient();
+const docClient = DynamoDBDocumentClient.from(dynamoDb);
+
+const scan = async () => {
+  const command = new ScanCommand({ TableName: TABLE_NAMES.PRODUCTS });
+  const products = await docClient.send(command);
+
+  return products;
+}
 
 const getProductsList = async () => formatJSONResponse({
-  message: products,
+  message: (await scan()).Items,
 });
 
 export const main = middyfy(getProductsList);
