4fdcff61c67f2bea43d696d3bc6ed81cd31b6f04
Kristof Poszet <kristof_poszet@epam.com>
2024-03-27 16:01:28 +0100

    feat: createProduct schema

--- product-service/src/utils/schema.ts
+++ product-service/src/utils/schema.ts
@@ -1,7 +1,24 @@
-export default {
-  type: "object",
-  properties: {
-    name: { type: 'string' }
+import * as Joi from "joi";
+
+export const schemas = {
+  json: {
+    createProduct: {
+      type: "object",
+      properties: {
+        count: { type: 'integer' },
+        price: { type: 'integer' },
+        title: { type: 'string' },
+        description: { type: 'string' }
+      },
+      required: ['count', 'price', 'title', 'description']
+    }
   },
-  required: ['name']
+  joi: {
+    createProduct: Joi.object({
+      count: Joi.number().integer().required(),
+      price: Joi.number().integer().required(),
+      title: Joi.string().min(1).max(64).required(),
+      description: Joi.string().min(1).max(256).required()
+    })
+  }
 } as const;
