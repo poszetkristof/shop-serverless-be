8e8ccad578efb0328d23cd92d45e747f3426e0a7
Kristof Poszet <kristof_poszet@epam.com>
2024-03-27 14:03:44 +0100

    feat: getProductsList error handling

--- product-service/src/functions/lambda/getProductsList.ts
+++ product-service/src/functions/lambda/getProductsList.ts
@@ -2,9 +2,15 @@ import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import DynamoDbService from "src/database/DynamoDbService";
 import { ProductDatabaseService } from 'src/database/ProductDatabaseService';
+import { ErrorMessages } from 'src/messages';
 
-const getProductsList = (productService: ProductDatabaseService) => async () => formatJSONResponse({
-  message: await productService.find(),
-});
+const getProductsList = (productService: ProductDatabaseService) => async () => {
+  try {
+    const products = await productService.find();
+    return formatJSONResponse({ message: products });
+  } catch {
+    return formatJSONResponse({ message: ErrorMessages.InternalServerError }, 500);
+  }
+}
 
 export const main = middyfy(getProductsList(DynamoDbService));
