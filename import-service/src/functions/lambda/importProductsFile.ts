a270177b00185bdeef1e5a215e181dd72129894c
Kristof Poszet <kristof_poszet@epam.com>
2024-04-06 09:27:06 +0200

    chore: configure prettier, winston logger, Api response builder

--- import-service/src/functions/lambda/importProductsFile.ts
+++ import-service/src/functions/lambda/importProductsFile.ts
@@ -1,10 +1,10 @@
-import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import type { APIGatewayProxyEvent } from 'aws-lambda';
+import { successResponse } from 'src/utils/apiResponseBuilder';
 
 const importProductsFile = async (event: APIGatewayProxyEvent) => {
-  return formatJSONResponse({
-    message: `Hello ${event.path}, welcome to the exciting Serverless world!`,
+  return successResponse({
+    message: `Hello more, welcome to the exciting Serverless world!`,
     event,
   });
 };
---
+++ import-service/src/utils/apiResponseBuilder.ts
@@ -0,0 +1,35 @@
+import { winstonLogger } from './winstonLogger';
+
+export interface ApiResponse {
+  statusCode: number;
+  headers: Object;
+  body: Object;
+}
+
+const defaultHeaders = {
+  'Access-Control-Allow-Methods': '*',
+  'Access-Control-Allow-Headers': '*',
+  'Access-Control-Allow-Origin': '*',
+};
+
+export const errorResponse = (err: Error, statusCode: number = 500): ApiResponse => {
+  winstonLogger.logError(`Error: ${err?.message}`);
+
+  return {
+    statusCode,
+    headers: {
+      ...defaultHeaders,
+    },
+    body: JSON.stringify({ message: err?.message ?? 'Something went wrong !!!' }),
+  };
+};
+
+export const successResponse = (body: Object, statusCode = 200): ApiResponse => {
+  winstonLogger.logRequest(`Lambda successfully invoked and finished`);
+
+  return {
+    statusCode,
+    headers: { ...defaultHeaders },
+    body: JSON.stringify(body),
+  };
+};
